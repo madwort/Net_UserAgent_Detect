6fb8db700d0908851bba12749e71f9a3cc57b6f8
Jason Rust <jrust@php.net>
2004-03-09 00:09:02 +0000

    * Using PEAR::getStaticProperty() to make all methods static again (but in a way that is php5 compatible) * Added a few new tests to the example/test file
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_UserAgent/trunk@153190 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/example.php
+++ tests/example.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../Detect.php';
-
 // {{{ some functions for printing
 
 if (!function_exists('println')) {
@@ -26,11 +25,13 @@ $_SERVER['HTTP_USER_AGENT'] = $GLOBALS['HTTP_USER_AGENT'] = 'Mozilla/5.0 (X11; U
 $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'fr;';
 
 $browserSearch = array('ie6up', 'ie5', 'ie4', 'gecko', 'ns6up', 'ns4', 'nav');
-$o_net =& Net_UserAgent_Detect::singleton();
-println('User Agent String: ' . $o_net->getUserAgent());
-println('Browser String: ' . $o_net->getBrowserString());
-println('OS String: ' . $o_net->getOSString());
-println('Browser flag: ' . $o_net->getBrowser($browserSearch));
+println('User Agent String: ' . Net_UserAgent_Detect::getUserAgent());
+println('Browser String: ' . Net_UserAgent_Detect::getBrowserString());
+println('OS String: ' . Net_UserAgent_Detect::getOSString());
+println('Browser flag: ' . Net_UserAgent_Detect::getBrowser($browserSearch));
+println('Has "popups disabled" quirk: ' . (Net_UserAgent_Detect::hasQuirk('popups_disabled') ? 'Yes' : 'No'));
+println('Has "dom" feature: ' . (Net_UserAgent_Detect::hasFeature('dom') ? 'Yes' : 'No'));
+println('Javascript version: ' . Net_UserAgent_Detect::getFeature('javascript'));
 $languages = array('fr', 'de', 'en');
-println('Accept Language: ' . $o_net->getAcceptType($languages, 'language'));
+println('Accept Language: ' . Net_UserAgent_Detect::getAcceptType($languages, 'language'));
 ?>
